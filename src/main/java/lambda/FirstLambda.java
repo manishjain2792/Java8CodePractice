603d26c4756f481fc2decb4c08bae52602cc4f27
Manish Jain <manish.jain@kpn.com>
2020-04-04 22:08:37 +0200

    First lambda expression code

---
+++ src/main/java/lambda/FirstLambda.java
@@ -0,0 +1,24 @@
+package lambda;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class FirstLambda {
+    public static void main(String[] args){
+        // Anonymous Class//
+//        FileFilter filter=new FileFilter() {
+//            public boolean accept(File pathname) {
+//                return pathname.getName().endsWith(".java");
+//            }
+//        };
+        FileFilter filterLambda=(File pathname) -> pathname.getName().endsWith(".java");
+
+        File dir= new File ("c:/mobiles");
+        File[] files= dir.listFiles(filterLambda);
+        if(files!=null)
+        for(File f :files){
+            System.out.println(f);
+        }
+
+    }
+}
